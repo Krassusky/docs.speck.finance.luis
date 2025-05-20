2f9c694b9081a4b6fe1dab0aeca97ce23c7625af
Krassusky <barroskrassusky@gmail.com>
2025-05-20 18:32:15 -0300

    Fixed khiza hyperlink

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -7,14 +7,22 @@ export const Partners: FC = () => {
       <div className="space-y-6">
         {/* Partners content will be added later */}
         <p className="text-gray-600 dark:text-gray-300">
-      <p className="mb-4">
-        We are proud to partner with the following organizations to ensure the
-        highest standards of security and compliance:
-        <a href="https://xdc.org/"><li>XDC Network</li></a>
-        <a href="https://khiza.com/"><li>Khiza</li></a>
-        <a href="https://fireblocks.io/"><li>Fireblocks</li></a>
-        <a href="https://blindpay.com/"><li>BlindPay</li></a>
-      </p>
+          <p className="mb-4">
+            We are proud to partner with the following organizations to ensure
+            the highest standards of security and compliance:
+            <a href="https://xdc.org/">
+              <li>XDC Network</li>
+            </a>
+            <a href="https://khizadao.com/">
+              <li>Khiza</li>
+            </a>
+            <a href="https://fireblocks.io/">
+              <li>Fireblocks</li>
+            </a>
+            <a href="https://blindpay.com/">
+              <li>BlindPay</li>
+            </a>
+          </p>
         </p>
       </div>
     </div>
