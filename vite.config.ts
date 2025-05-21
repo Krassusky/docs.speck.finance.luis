0b352a24023a978e3b671e9b76d66ff9edd67a32
Krassusky <barroskrassusky@gmail.com>
2025-05-20 21:38:58 -0300

    Fixed bug on refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,11 +12,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    historyApiFallback: true,
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ['**/*.svg', '**/*.jpg', '**/*.jpeg'],
-})
+  assetsInclude: ["**/*.svg", "**/*.jpg", "**/*.jpeg"],
+});
